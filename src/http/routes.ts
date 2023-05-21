6ae405765beb45fe5d67bb3146585129f9fc0506
Marcelo Guimaraes da Silva <marceloguimaraes@outlook.com.br>
2023-05-21 02:05:36 -0300

    lint: format

---
+++ src/http/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify';
+import { authenticate, register } from './controllers';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', register);
+  app.post('/sessions', authenticate);
+}
