6dd15ee6169f592a67119551f07eb7d209071cc8
Marcelo Guimaraes da Silva <marceloguimaraes@outlook.com.br>
2023-11-20 00:30:01 -0300

    lint: remove unecessary import

--- src/use-cases/get-user-metrics.ts
+++ src/use-cases/get-user-metrics.ts
@@ -1,5 +1,4 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
-import { CheckIn } from '@prisma/client';
 import { ResourceNotFoundError } from './errors/resource-not-found';
 
 interface GetUserMetricsUseCaseBody {
